d000f837e1490e05083e336eb61e1f7477475cf7
Artem <artem200@yandex.ru>
2016-03-18 12:46:41 +0300

    material show

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from .models import Material
 
 # Create your views here.
+
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {'posts': Material.objects.all()})
